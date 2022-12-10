95debb1827bf898d757d1ded985dcda56ec57859
Poioill <satkynovaz@gmail.com>
2022-12-10 15:32:21 +0600

    login_signup

--- main/settings.py
+++ main/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quiz',
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "accounts.CustomUser"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quiz.urls'))
+    path('', include('quiz.urls')),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
