c2aaedebdab0f5e0e262de8e85327a05935071a3
Poioill <satkynovaz@gmail.com>
2022-12-12 22:18:34 +0600

    subject_materila_for_Asem_2

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('', Categories.as_view(), name='category'),
     path('categories/<int:pk>/', SubjectsDetailView.as_view(), name='subjects'),
+    path('materials/', MaterialsTemplateView.as_view(), name='materials'),
+    path('materials/', SubjectMaterialListView.as_view(), name='subjectmaterial'),
+    path('privacy/', PrivacyTemplateView.as_view(), name='privacy'),
     #path('categories/<int:pk>/', Area.as_view(), name='subject_areas'),
     #path('materials/<int:pk>/', SubjectMaterialView.as_view(), name='subjects'),
 ]
