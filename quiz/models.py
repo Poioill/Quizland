ca4fb900472a85a664eda75b16afc3bc71d83841
Poioill <satkynovaz@gmail.com>
2022-12-13 10:29:24 +0600

    subject_materila_for_Asem_4

--- quiz/models.py
+++ quiz/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.core.validators import FileExtensionValidator
-from django.contrib.auth.models import User
+from accounts.models import CustomUser
+
 
 class Answers(models.Model):
     question = models.CharField(max_length=50, null=True)
@@ -10,8 +11,10 @@ class Answers(models.Model):
 
     def __str__(self):
         return self.question
+
     class Meta:
         verbose_name_plural = 'Answers'
+
 
 class Quiz_category(models.Model):
     category_name = models.CharField(max_length=50, null=True)
@@ -42,8 +45,12 @@ class SubjectMaterial(models.Model):
 
 
 class Comments(models.Model):
-    comment = models.ForeignKey(Quiz_category, on_delete=models.CASCADE, blank=True, null=True, related_name='comments_quiz_category')
+    comment = models.ForeignKey(Quiz_category, on_delete=models.CASCADE, blank=True, null=True,
+                                related_name='comments_quiz_category')
     comment_text = models.TextField()
-    creation_date = models.DateTimeField(auto_now = True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, verbose_name='Автор комметария', null=True)
+    status = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(auto_now=True)
+
     class Meta:
-        verbose_name_plural='Comments'
+        verbose_name_plural = 'Comments'
